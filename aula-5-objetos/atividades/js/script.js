f833ae0cb8d2fba23647cb95bcd06743e4b2674f
fellran <>
2023-11-27 17:44:15 -0300

    atividade-5-Objetos
    tentando fazer a atividade

--- aula-5-objetos/atividades/js/script.js
+++ aula-5-objetos/atividades/js/script.js
@@ -6,19 +6,32 @@
 // cor: string
 // velocidadeMaxima: número
 // velocidadeAtual: número (inicialmente 0)
+
 class Carro {
-    constructor(){
+    constructor(carroMarca, carroModelo, carroAno, carroVelocidadeMaxima, carroVelocidadeAtual){
         this.marca = carroMarca;
         this.modelo = carroModelo;
         this.ano = carroAno;
         this.velocidadeMaxima = carroVelocidadeMaxima;
         this.velocidadeAtual = carroVelocidadeAtual;
-
     }
-}
 
 // Em seguida, crie um método chamado "acelerar" que receba um número como argumento e aumente a velocidade atual do carro por esse número.Certifique-se de que a velocidade atual não ultrapasse a velocidade
 // máxima do carro.
-function acelerar(){
-
+    acelerar(aumentar){
+        if (aumentar < carroVelocidadeMaxima){
+            this.aumentar = this.aumentar + carroVelocidadeAtual
+            return("velocidade adicionada");
+        }
+        else{
+            return("limite de velocidade maxima utrapassado");
+        }
+    };
 };
+
+var carro1 = new Carro("Fiat", "Uno", "1999", 300, 100);
+
+
+
+
+
