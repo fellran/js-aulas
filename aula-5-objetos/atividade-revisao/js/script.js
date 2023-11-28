540d485176892ebfba7a1e6add25beb9521992e4
fellran <>
2023-11-28 19:15:29 -0300

    atividade-revisao
    terminando a atividade revisao

--- aula-5-objetos/atividade-revisao/js/script.js
+++ aula-5-objetos/atividade-revisao/js/script.js
@@ -2,10 +2,11 @@
 // descrição,data, diretor e categoria e liste ela de alguma forma em algum elemento HTML!
 
 class Filme {
-    constructor(nomeFilme, descricaoFilme, dataFilme, categoriaFilme){
+    constructor(nomeFilme, descricaoFilme, dataFilme, diretorFilme ,categoriaFilme){
         this.nome = nomeFilme;
         this.descricao = descricaoFilme;
         this.data = dataFilme;
+        this.diretor = diretorFilme;
         this.categoria = categoriaFilme
     }
 
@@ -20,13 +21,19 @@ function cadastro(){
    var nomeInput = document.querySelector("#inome").value;
    var descricaoInput = document.querySelector("#idescricao").value;
    var dataInput = document.querySelector("#idata").value;
+   var diretorInput = document.querySelector("#idiretor").value;
    var categoriaInput = document.querySelector("#icategoria").value;
-   var resultado = document.querySelector("#resultado");
+   var resposta = document.querySelector("#resposta");
 
     // PROCESSAR DADOS
-    var filme = new Filme(nomeInput, descricaoInput, dataInput, categoriaInput);
+    var filme = new Filme(nomeInput, descricaoInput, dataInput, diretorInput ,categoriaInput);
 
     // SAIDA DE DADOS
     resposta.innerHTML = `<div class="post">
+                            <h3>Nome - ${filme.nome}</h3>
+                            <p>Descricao - ${filme.descricao}</p>
+                            <p>Data - ${filme.data}</p>
+                            <p>Diretor - ${filme.diretor}</p>
+                            <p>Categoria - ${filme.categoria}</p>
                             </div>`
 };
